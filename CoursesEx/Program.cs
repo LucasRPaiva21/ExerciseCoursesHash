ff5be3175efeca40c78480cb31efdae10f5b7685
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-06-21 00:03:44 -0300

    Versão Final

--- CoursesEx/Program.cs
+++ CoursesEx/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CoursesEx
 {
@@ -6,7 +7,41 @@ namespace CoursesEx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<int> courseA = new HashSet<int>();
+            HashSet<int> courseB = new HashSet<int>();
+            HashSet<int> courseC = new HashSet<int>();
+
+            Console.Write("How many students for course A? ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                int iD = int.Parse(Console.ReadLine());
+                courseA.Add(iD);
+            }
+
+            Console.Write("How many students for course B? ");
+            int o = int.Parse(Console.ReadLine());
+            for (int i = 0; i < o; i++)
+            {
+                int iD = int.Parse(Console.ReadLine());
+                courseB.Add(iD);
+            }
+
+            Console.Write("How many students for course C? ");
+            int p = int.Parse(Console.ReadLine());
+            for (int i = 0; i < p; i++)
+            {
+                int iD = int.Parse(Console.ReadLine());
+                courseC.Add(iD);
+            }
+
+            HashSet<int> all = new HashSet<int>(courseA);
+            all.UnionWith(courseB);
+            all.UnionWith(courseC);
+            Console.WriteLine("Total students: " + all.Count);
+
+
+
         }
     }
 }
